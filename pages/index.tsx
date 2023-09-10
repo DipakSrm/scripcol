5a955b989047a85e8d0e02f821a60f869fea0b16
Dipak99990 <dipaksrm98@gmail.com>
2023-09-10 23:57:17 +0545

    egvews

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,27 @@
-import ReactDOM from "react-dom";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useAuth } from "./hooks/authenication";
+import SignUpForm from "./signup";
 
-import axios from "axios";
-import { User } from "@/utils/interfaces";
-
-export default function App() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<User>();
-  const onSubmit: SubmitHandler<User> = async (data) => {
-    try {
-      const response = await axios.post("/api/user/", {
-        Name: data.Name,
-        Email: data.Email,
-        Password: data.Password,
-      });
-      if (response.status === 201) {
-        console.log(response);
-        console.log("sent sucessfully");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+export default function index() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { user, loading, signOut } = useAuth();
+  if (loading) {
+    return <p>Loading...</p>;
+  }
   return (
-    <div className="bg-slate-200 h-screen">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col mx-auto p-4 w-1/2 bg-white"
-      >
-        <label>First Name</label>
-        <input {...register("Name")} className="bg-slate-300" />
-        <label htmlFor="email">Email</label>
-        <input
-          {...(register("Email"), errors)}
-          id="email"
-          className="bg-slate-300"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          {...(register("Password"), {})}
-          id="password"
-          className="bg-slate-300"
-        />
-        <button type="submit" className="bg-black text-white">
-          Get Started
+    <>
+      {!user && (
+        <div className="flex flex-col gap-5 ">
+          <SignUpForm />
+        </div>
+      )}
+      {user && (
+        <button
+          className="text-white bg-blue-600 p-4 w-50% text-lg"
+          onClick={() => signOut()}
+        >
+          LogOut{" "}
         </button>
-      </form>
-    </div>
+      )}
+    </>
   );
 }
